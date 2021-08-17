033e30c9adcbd0d4017b31409dc01786da8119a1
James <jamessoh11@gmail.com>
2021-08-17 11:49:12 +0800

    Level-1 remove greeter class, refactor interactor class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
+
         Operations operations = new Operations();
         Interactor interactor = new Interactor();
         interactor.setOperations(operations);
-
-        greeter.printGreeting();
-        interactor.start("bye");
-        greeter.printGoodbye();
+        interactor.setExitKeyword("bye");
+        interactor.start();
 
 
     }
--- src/main/java/Interactor.java
+++ src/main/java/Interactor.java
@@ -1,24 +1,49 @@
 import java.util.Scanner;
 
 public class Interactor {
+
+    Operations operations;
+    String exitKeyword;
+
     public Interactor() {
     }
-    Operations operations;
 
     public void setOperations(Operations operations) {
         this.operations = operations;
     }
 
-    public void start(String exitOn){
-        String message = String.format("Please type %s if you want to exit.", exitOn);
+    public void setExitKeyword(String exitKeyword){
+        this.exitKeyword = exitKeyword;
+    }
+
+    public void printGreeting(){
+        System.out.println("Dunder Mifflin, this is Pam");
+    }
+
+    public void printGoodbye(){
+        System.out.println("Okay bye");
+    }
+
+    public void start(){
+        this.printGreeting();
+        this.printExitKeyword();
+
+        boolean toContinue = true;
         Scanner scanner = new Scanner(System.in);
-        while (true){
+        while (toContinue == true){
             String currentInput = scanner.nextLine();
-            if (currentInput == exitOn){
+            if (currentInput.equals(this.exitKeyword)){
+                this.printGoodbye();
                 break;
             }
             String result = this.operations.echo(currentInput);
             System.out.println(result);
+
         }
+    }
+
+    private void printExitKeyword() {
+        String message = String.format("Please type %s if you want to exit.", this.exitKeyword);
+        System.out.println(message);
     }
 }
